fb519f74e8a496de021268754823e23af135579b
Victoria0292 <tlynn0292@gmail.com>
2022-11-10 11:22:15 -0500

    linked script to HTML and started writing the JS

---
+++ script.js
@@ -0,0 +1,8 @@
+const numbers = document.querySelectorAll('.number')
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+
+let activeNumber = 1;
+
+
