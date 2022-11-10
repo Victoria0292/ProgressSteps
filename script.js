38e1dde2f3f0edacaab1bd89935374256102d83c
Victoria0292 <tlynn0292@gmail.com>
2022-11-10 14:08:09 -0500

    struggled with correct css for progress bar. Working through tutorial for guidance

--- script.js
+++ script.js
@@ -5,4 +5,37 @@ const next = document.getElementById('next')
 
 let activeNumber = 1;
 
+next.addEventListener('click', () => {
+    activeNumber++
 
+    if (activeNumber > numbers.length) {
+        activeNumber = numbers.length
+    }
+
+    loading()
+})
+
+prev.addEventListener('click', () => {
+    activeNumber--
+
+    if (activeNumber < 1) {
+        activeNumber = 1
+    }
+
+    loading()
+})
+
+
+function loading() {
+    numbers.forEach((number, anId) => {
+        if (anId < activeNumber) {
+            number.classList.add('active')
+        } else {
+            number.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length - 1) / (numbers.length -1) * 100 + '%'
+}
